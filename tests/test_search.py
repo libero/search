2243459fca2d988c62da42e0bf95896a0a0ad27f
Giancarlo Fusiello <carlofusiello@gmail.com>
2019-02-05 14:45:07 +0000

    rewrite empty response test to use schema validation (#3)

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,24 +1,26 @@
+import os
 from io import BytesIO
 
 from lxml import etree
 
 
 def test_empty_response(client) -> None:
+
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                        'schemas',
+                        'item-list.xsd')
+
+    with open(path, 'rb') as xmlfile:
+        xmlschema_doc = etree.XML(xmlfile.read())
+
+    xmlschema = etree.XMLSchema(xmlschema_doc)
+
     response = client.get('/search')
-    xml = etree.parse(BytesIO(response.data))
-    root = xml.getroot()
-
-    assert root.getchildren() == []
-
-
-def test_empty_response_is_well_formed(client) -> None:
-    response = client.get('/search')
-    xml = etree.parse(BytesIO(response.data))
-    root = xml.getroot()
-
     assert response.status_code == 200
     assert response.content_type == 'application/xml; charset=utf-8'
-    assert xml.docinfo.encoding == 'UTF-8'
-    assert xml.docinfo.xml_version == '1.0'
-    assert xml.docinfo.root_name == 'item-list'
-    assert root.tag == '{http://libero.pub}item-list'  # check namespace
+
+    xml = etree.parse(BytesIO(response.data))
+    root = xml.getroot()
+
+    assert xmlschema.validate(xml) is True, xmlschema.error_log
+    assert root.getchildren() == []
