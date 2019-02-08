bcb8834000e5e35d65b65175dbfc899aec81d6e5
Giancarlo Fusiello <carlofusiello@gmail.com>
2019-02-08 09:45:11 +0000

    implement ping endpoint (#4)

--- search/__init__.py
+++ search/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
+from search.ping import get_ping_blueprint
 from search.search import get_search_blueprint
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.register_blueprint(get_search_blueprint())
+    app.register_blueprint(get_ping_blueprint())
     return app
